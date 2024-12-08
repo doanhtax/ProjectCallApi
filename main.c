92666b0aeb1de342774ee3f4dcc70b2f33396854
Nguyễn Đỗ Anh Tài <141052628+doanhtax@users.noreply.github.com>
2024-12-08 10:04:43 +0700

    reconfigure taskjson, c_cpp, add associated certificate cacert.pem

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curl/curl.h>
 
-int main(void) {
+int main()
+{
     CURL *curl;
     CURLcode res;
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.example.com");
-        res = curl_easy_perform(curl); // Gửi yêu cầu HTTP
+    curl = curl_easy_init(); // Khởi tạo cURL
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");                   // Đặt URL
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);                              // Cho phép chuyển hướng
+        curl_easy_setopt(curl, CURLOPT_CAINFO, "D:/Project_Call_API_Google/cacert.pem"); // Đặt file CA certificate
+        res = curl_easy_perform(curl);                                                   // Thực hiện request
         if (res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
+        {
+            fprintf(stderr, "cURL failed: %s\n", curl_easy_strerror(res));
+        }
+        else
+        {
+            printf("cURL is working correctly!\n");
+        }
+
+        curl_easy_cleanup(curl); // Dọn dẹp
     }
-    curl_global_cleanup();
+    else
+    {
+        printf("Failed to initialize cURL.\n");
+    }
+    system("pause"); // Dừng lại cho đến khi nhấn phím bất kỳ (Windows)
+
     return 0;
 }
